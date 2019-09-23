3c2682d82be4e10c0d894da50f63220f582a8c83
Mauricio Degregori <degregori2@gmail.com>
2019-09-23 16:15:41 -0500

    log in and signup forms

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, {useState, useEffect} from "react";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
 function Signup() {
     return (
@@ -9,3 +12,70 @@ function Signup() {
 }
 
 export default Signup
+
+const Signup=({errors, touched, status}) => {
+    const [newUser,setNewUser] = useState([]);
+    useEffect(() => {
+        if (status){
+            setNewUser([...newUser, status]);
+        }
+    },[status]);
+
+    return(
+
+    <div>
+        <Form>
+            <div>
+                Name: <Field type= "text" name="name" placeholder="enter full name"/>
+                {touched.name && errors.name && (
+                    <p>{errors.name}</p>
+                )}
+                Email: <Field type="text" name="email" placeholder="enter email address"/>
+                {touched.email && errors.email && (
+                    <p>{errors.email}</p>
+                )}
+                Password: <Field type="password" name="password" placeholder="enter your password"/>
+                {touched.password && errors.password && (
+                    <p>{errors.password}</p>
+                )}
+            </div>
+        </Form>
+        {users.map(event => (
+        <ul className = "list"key={event.id}>
+          <li>Name:{e.name}</li>
+          <li>Email: {event.email}</li>
+          <li>Password: {event.password} </li>
+        </ul>
+      ))}
+    </div>
+    );
+};
+const FormikSignup = withFormik({
+    mapPropsToValues({email,password}){
+        return {
+            name: name || "",
+            email: email ||"",
+            password: password || "",
+        };
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required("You must enter your name before continuing"),
+        email: Yup.string().required("Email must be entered"),
+        password: Yup.string().required("Password must be entered"),       
+    }),
+    handleSubmit(values, { setStatus }){
+    axios
+    .post("https://reqres.in/api/users/", values)
+    .then(event => {
+        setStatus(event.data);
+        console.log(event.data)
+    })
+    .catch(err => console.log(err.e));
+
+
+
+}
+
+})(Signup);
+export default FormikSignup
+
