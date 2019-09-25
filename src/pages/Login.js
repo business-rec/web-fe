1d824f6240ec70b9e4d62f54814b00adf2fabe0e
Mauricio Degregori <degregori2@gmail.com>
2019-09-25 18:00:12 -0500

    formik code test

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,100 +1,102 @@
-import React, {useState} from "react";
-import axios from 'axios';
+import React, {useState, useEffect} from "react";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 import styled from 'styled-components'
 
-const Login = (props) => {
- 
-  const [credentials, setCredentials] = useState({
-    username: '',
-    password: ''
-  })
+const Login=({errors, touched, status}) => {
+    const [users,setUsers] = useState([]);
+    useEffect(() => {
+        if (status){
+            setUsers([...users, status]);
+        }
+    },[status]);
 
-  const handleChange = e => {
-    setCredentials({
-      ...credentials,
-      [e.target.name]: e.target.value
-    })
-  }
+    return(
 
-  const onLogin = e => {
-    e.preventDefault();
-    axios
-      .post('https://business-rec-web-be.herokuapp.com/api/auth/login', credentials)
-      .then(res => {
-        localStorage.setItem('token', res.data.payload)
-        props.history.push('/home')
-      })
-  }
-  
-  return (
     <Main>
-      <Form onSubmit={onLogin}>
-      <Header>Login</Header>
-        <Container>
-          <Input
-            type="text"
-            name="username"
-            placeholder="Username"
-            value={credentials.username}
-            onChange={handleChange}
-          />
-          <Input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={credentials.password}
-            onChange={handleChange}
-          />
-        </Container>
-        <Bcontainer>
-          <Button>Log In</Button>
-        </Bcontainer>
-      </Form>
+        <Form>
+            <Container>
+                <Title>Log In</Title>
+                <Text>
+                  <Field type="text" name="username" placeholder="username"/>
+                  {touched.username && errors.username && (
+                      <p>{errors.username}</p>
+                  )}
+                  <Field type="password" name="password" placeholder="password"/>
+                  {touched.password && errors.password && (
+                      <p>{errors.password}</p>
+                  )}
+                </Text>
+                <Button>Sign In</Button>
+            </Container>
+        </Form>
     </Main>
-  );
+    );
 };
+const FormikLogin = withFormik({
+    mapPropsToValues({username,password}){
+        return {
+            username: username || "",
+            password: password || ""
+        };
+    },
+    validationSchema: Yup.object().shape({
+        username: Yup.string().required("Username must be entered"),
+        password: Yup.string().required("Password must be entered")    
+    }),
+    handleSubmit(values, { props, setStatus }){
+    axios
+    .post("https://business-rec-web-be.herokuapp.com/api/auth/login", values)
+    .then(event => {
+        setStatus(event.data);
+            props.history.push('/home')
+    })
+    .catch(err => console.log(err.e));
 
-export default Login;
+}
 
-//styled
+})(Login);
+console.log(FormikLogin)
+export default FormikLogin;
+
+//styling
 
 const Main = styled.div`
   background-color: #F5F5F5;
-
   height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
 `
-const Form = styled.form`
-  border: 1px solid black;
-  background-color: #ffffff;
-  padding: 20px;
-`
-const Header = styled.h1`
-  text-align: center;
-  margin: 10%;
-`
+
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 60px;
-`
-const Input = styled.input`
- margin: 5%;
- width: 100%;
-`
-const Bcontainer = styled.div`
+border:1px solid black;
+padding: 30%;
 display: flex;
 align-items: center;
 justify-content: center;
+flex-direction: column;
+background-color: #fff;
 `
+
+const Text = styled.div`
+margin: 25%;
+padding: 10%;
+
+`
+
 const Button = styled.button`
+
+width: 100%;
+color: #ffffff;
 background-color: black;
-padding: 6%;
-margin: 8%;
-color: white;
-width: 75%;
+padding: 10%;
+
+
+`
+const Title = styled.label`
 text-align: center;
+margin: 10%;
 `
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,113 +1,102 @@
-import React, {useState} from "react";
-import axios from 'axios';
+import React, {useState, useEffect} from "react";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 import styled from 'styled-components'
 
+const Signup=({errors, touched, status}) => {
+    const [newUser,setNewUser] = useState([]);
+    useEffect(() => {
+        if (status){
+            setNewUser([...newUser, status]);
+        }
+    },[status]);
 
-const Signup = (props) => {
- 
-  const [credentials, setCredentials] = useState({
-    username: '',
-    email: '',
-    password: ''
-  })
+    return(
 
-  const handleChange = e => {
-    setCredentials({
-      ...credentials,
-      [e.target.name]: e.target.value
-    })
-  }
-
-  const onSignup = e => {
-    e.preventDefault();
-    axios
-      .post('https://business-rec-web-be.herokuapp.com/api/auth/register', credentials)
-      .then(res => {
-        localStorage.setItem('token', res.data.payload)
-        props.history.push('/')
-      })
-    
-  }
-
-  return (
     <Main>
-      <Form onSubmit={onSignup}>
-      <Header>Signup</Header>
-      <Container>
-          <Input
-            type="text"
-            name="username"
-            placeholder="Username"
-            value={credentials.username}
-            onChange={handleChange}
-          />
-
-          <Input
-            type="text"
-            name="email"
-            placeholder="Email"
-            value={credentials.email}
-            onChange={handleChange}
-          />
-          
-          <Input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={credentials.password}
-            onChange={handleChange}
-          />
-        </Container>
-        <Bcontainer>
-          <Button >Sign up</Button>
-        </Bcontainer>
-      </Form>
+        <Form>
+            <Container>
+                <Title>Sign Up</Title>
+                <Text>
+                  <Field type= "text" name="username" placeholder="username"/>
+                  {touched.username && errors.username && (
+                      <p>{errors.username}</p>
+                  )}
+                  <Field type="password" name="password" placeholder="password"/>
+                  {touched.password && errors.password && (
+                      <p>{errors.password}</p>
+                  )}
+                </Text>
+                <Button>Sign Up</Button>
+            </Container>
+        </Form>
     </Main>
-  );
+    );
 };
+const FormikSignup = withFormik({
+    mapPropsToValues({username,password}){
+        return {
+            username: username || "",
+            password: password || ""
+        };
+    },
+    validationSchema: Yup.object().shape({
+        username: Yup.string().required("You must enter your name before continuing"),
+        password: Yup.string().required("Password must be entered")       
+    }),
+    handleSubmit(values,  {props, setStatus }){
+    axios
+    .post("https://business-rec-web-be.herokuapp.com/api/auth/register", values)
+    .then(event => {
+        setStatus(event.data);
+            props.history.push('/')
+    })
+    .catch(err => console.log(err.e));
 
-export default Signup;
+}
 
-//styled
+})(Signup);
+console.log(FormikSignup)
+export default FormikSignup
+
+//styles
 
 const Main = styled.div`
   background-color: #F5F5F5;
-
   height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
 `
-const Form = styled.form`
-  border: 1px solid black;
-  background-color: #ffffff;
-  padding: 20px;
-`
-const Header = styled.h1`
-  text-align: center;
-  margin: 10%;
-`
+
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 60px;
-`
-const Input = styled.input`
- margin: 5%;
- width: 100%;
-`
-const Bcontainer = styled.div`
+border:1px solid black;
+padding: 30%;
 display: flex;
 align-items: center;
 justify-content: center;
+flex-direction: column;
+background-color: #fff;
+`
+
+const Text = styled.div`
+margin: 25%;
+padding: 10%;
 
 `
+
 const Button = styled.button`
+
+width: 100%;
+color: #ffffff;
 background-color: black;
-padding: 6%;
-margin: 8%;
-color: white;
-width: 75%;
+padding: 10%;
+
+
+`
+const Title = styled.label`
 text-align: center;
+margin: 10%;
 `
